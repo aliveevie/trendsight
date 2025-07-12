9a104c08ccb9f94e9bdff6ce4f945e2716125bf3
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2025-07-12 11:29:22 +0100

    Feat: update files

--- server/trend_following_server.py
+++ server/trend_following_server.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Allow CORS for local frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["http://localhost:8080", "https://trendsight-server.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"] ,
     allow_headers=["*"]
